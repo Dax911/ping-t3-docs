5104f01c406cdbe686628edd20f737d3ffa03c31
Dax <haydenaylor@icloud.com>
2022-04-03 03:25:56 -0400

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -4,9 +4,9 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Ping</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-          Ping
+          A better way to stream
       </span>
     </>
   ),
